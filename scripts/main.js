7e661f0ac2bd5fefe6268fc158aacc0ff0fe1ae5
ali.shariati <a.shariatii91@gmail.com>
2020-10-30 06:36:55 -0400

    completed html, css, added a funciton for an interactive keyboard.

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,26 @@
 "use strict";
+
+// hiding/uhiding the keyboard
+const input = document.getElementsByClassName("input")[0].childNodes[1];
+const keyboard = document.getElementsByClassName("keyboard")[0];
+
+input.addEventListener("click", (ev) => {
+    keyboard.style.bottom = "0";
+    window.location.hash = "jumpToInput";
+    // to prevent bubbling up to parent element for hiding the keyboard
+    ev.stopPropagation();
+}, false);
+
+keyboard.addEventListener("click", (ev) => {
+    // to prevent bubbling up to parent element for hiding the keyboard
+    ev.stopPropagation();
+}, false);
+
+window.addEventListener("click", () => {
+    window.location.hash = "jumpToTheTop";
+    keyboard.style.bottom = "-100vh";
+});
+
+
+// append with one key
+// create one key
